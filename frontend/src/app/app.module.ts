5c488c4e46ecf36ce684642f9957b410d6d33406
Piotr Kwiatkowski <piotr.kwiatkowski@pb.com>
2018-03-26 16:41:40 +0200

    adding some style

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatInputModule,
+  MatSnackBarModule,
+  MatToolbarModule
+} from '@angular/material';
 
 import { MessagesComponent } from './messages.components';
 import { AppComponent } from './app.component';
@@ -12,9 +18,16 @@ import { AppComponent } from './app.component';
     AppComponent, MessagesComponent
   ],
   imports: [
-    BrowserModule, BrowserAnimationsModule, MatButtonModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatSnackBarModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
